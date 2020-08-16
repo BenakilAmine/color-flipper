a3b5f24a79203618ae83c3c772fb0eed6dc26e5e
Amine BENAKIL <47107417+BenakilAmine@users.noreply.github.com>
2020-08-17 00:04:29 +0200

    Base changement du background par un click

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+// Evenement clique qui a comme fonction de changer le background
+
+btn.addEventListener('click', function() {
+    //test
+    console.log(document.body);
+    // get random number entre 0 - 3
+    const randomNumber = 1;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
